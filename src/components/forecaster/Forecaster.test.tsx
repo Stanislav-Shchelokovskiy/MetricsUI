ac9d6e524f8d5046f506b7b714591fbcf48041cf
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-11-07 21:19:26 +0400

    add test

--- src/components/forecaster/Forecaster.test.tsx
+++ src/components/forecaster/Forecaster.test.tsx
@@ -1,8 +1,6 @@
-import React, { PropsWithChildren } from 'react';
-import { screen, render, fireEvent } from '@testing-library/react';
+import React from 'react';
+import { screen, render } from '@testing-library/react';
 import Forecaster from './Forecaster';
-import TribesSelector from './menu/CommonSettingsPanel'
-import TagBox, { ITagBoxOptions } from 'devextreme-react/tag-box'
 
 const fetchForecastSettingsValues = require('./network_resource_fetcher/FetchForecastSettingsValues')
 const tribes = [{ 'id': '46ABE8AA-E6BB-4D47-AAC6-AADF8B689619', 'name': 'App Frameworks (UI, API, ORM)' }, { 'id': '44763D87-A7DD-499B-8D43-92E767D5BA0E', 'name': 'ASP' }]
@@ -32,18 +30,3 @@ test('initial state', async () => {
     expect(screen.getByTestId('LoadIndicator')).toBeInTheDocument()
     expect(await screen.findByTestId('Menu')).toBeInTheDocument()
 })
-
-
-
-// test('select tribe', async () => {
-//     let changeEvent: ((value: any) => void) | undefined
-//     jest.mock('devextreme-react/tag-box', () => {
-//         return function (props: PropsWithChildren<ITagBoxOptions>) {
-//             changeEvent = props.onValueChange
-//             return jest.fn()
-//         }
-//     })
-//     render(<Forecaster />)
-//     changeEvent?.(tribes)
-//     expect(await screen.findByTestId('TribesContainer', undefined, { timeout: 1000, interval: 500 })).toBeInTheDocument()
-// })
