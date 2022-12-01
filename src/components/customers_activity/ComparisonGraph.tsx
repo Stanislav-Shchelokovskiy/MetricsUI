7529d1f4978dfd451b101beeef390724c515033e
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-12-01 17:55:59 +0400

    fix error

--- src/components/customers_activity/ComparisonGraph.tsx
+++ src/components/customers_activity/ComparisonGraph.tsx
@@ -37,10 +37,10 @@ export default function ComparisonGraph() {
                     customersActivityState.groupByPeriod,
                     customersActivityState.range[0],
                     customersActivityState.range[1],
-                    set.customersGroups.map(group => group.id),
-                    set.ticketsTypes.map(type => type.id),
-                    set.ticketsTags.map(tag => tag.id),
-                    set.tribes.map(tribe => tribe.id),
+                    set.customersGroups?.map(group => group.id) || [],
+                    set.ticketsTypes?.map(type => type.id) || [],
+                    set.ticketsTags?.map(tag => tag.id) || [],
+                    set.tribes?.map(tribe => tribe.id) || [],
                 )
                 if (fetchedAggregates.success) {
                     aggs.push({
