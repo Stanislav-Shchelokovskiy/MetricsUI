bc04b52430ec92f1ab1a4c84b3c0802ffe107202
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-12-14 13:34:43 +0400

    refactor download button

--- src/components/customers_activity/toolbar/Toolbar.tsx
+++ src/components/customers_activity/toolbar/Toolbar.tsx
@@ -1,45 +1,15 @@
-import React, { useCallback, useState } from 'react'
-import { Toast } from 'devextreme-react/toast';
+import React from 'react'
 import Button from '../../common/components/Button'
 import DownloadButton from './DownloadButton'
 
 function Toolbar({ onShowChange }: { onShowChange: () => void }) {
-    const [toastConfig, setToastConfig] = useState({
-        isVisible: false,
-        message: '',
-    })
-
-    const onError = useCallback(
-        (message: string) => {
-            setToastConfig({
-                message: message,
-                isVisible: true,
-            });
-        }, [])
-
-    const onHiding = useCallback(() => {
-        setToastConfig({
-            ...toastConfig,
-            isVisible: false,
-        });
-    }, [])
-
     return (
-        <React.Fragment>
-            < div className='CustomersActivityToolbar'>
-                <Button icon='menu' onClick={onShowChange} />
-                <div className='CustomersActivityToolbarCommands'>
-                    <DownloadButton onError={onError} />
-                </div>
+        < div className='CustomersActivityToolbar'>
+            <Button icon='menu' onClick={onShowChange} />
+            <div className='CustomersActivityToolbarCommands'>
+                <DownloadButton />
             </div>
-            <Toast
-                visible={toastConfig.isVisible}
-                message={toastConfig.message}
-                type='error'
-                onHiding={onHiding}
-                displayTime={3000}
-            />
-        </React.Fragment >
+        </div>
     )
 }
 
