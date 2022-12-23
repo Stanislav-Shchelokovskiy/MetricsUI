3fde2243c586f6dd858d4e46ec0c630ba34a7742
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-12-23 14:27:10 +0400

    reset state instead of dropping it is the set is last

--- src/components/customers_activity/content/set/header/DropButton.tsx
+++ src/components/customers_activity/content/set/header/DropButton.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import Button from '../../../../common/components/Button'
-import { CustomersActivityStore } from '../../../store/Store'
 import { removeSet } from '../../../store/Actions'
 
 
@@ -11,10 +10,9 @@ export default function DropButton({ setTitle }: { setTitle: string }) {
         dispatch(removeSet(setTitle))
     }, [setTitle, dispatch])
 
-    const lastSet = useSelector((state: CustomersActivityStore) => state.customersActivity.sets).length === 1
     return <Button
         className='SetHeaderButton'
-        hint={lastSet ? 'Reset' : 'Drop'}
+        hint='Drop'
         icon='remove'
         stylingMode='text'
         onClick={onClick}
