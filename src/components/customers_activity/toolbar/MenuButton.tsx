b8abdcac42bd02ccd86edbc8ba14e372bfbf890f
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-01-31 17:14:51 +0400

    encrease tooltip time

--- src/components/customers_activity/toolbar/MenuButton.tsx
+++ src/components/customers_activity/toolbar/MenuButton.tsx
@@ -21,7 +21,7 @@ export default function MenuButton(props: Props) {
         timerId.current = setTimeout(() => {
             setFilterTooltipVisible(true)
             clearTimeout(timerId.current)
-        }, 500)
+        }, 600)
     }
     const onLeave = () => {
         clearTimeout(timerId.current)
