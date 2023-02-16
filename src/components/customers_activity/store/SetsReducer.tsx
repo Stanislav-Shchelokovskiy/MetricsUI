82cdfd5204dc57d3078913bd30d6da855db5a6eb
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-02-16 19:22:21 +0400

    fix undefined values when not inclueded

--- src/components/customers_activity/store/SetsReducer.tsx
+++ src/components/customers_activity/store/SetsReducer.tsx
@@ -661,7 +661,7 @@ function updateValues<T>(obj: FilterParametersNode<T> | undefined, values: Array
         return undefined
     return {
         ...obj,
-        values: values
+        values: values || []
     }
 }
 
