18876d8b3c5838a82d158ae7dd4a93cdd3973ae5
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-05-05 17:43:59 +0400

    refactor

--- src/components/customers_activity/store/StoreStateValidator.tsx
+++ src/components/customers_activity/store/StoreStateValidator.tsx
@@ -21,10 +21,10 @@ export function validateCustomersActivitySetsProperties(customersActivitySets: A
 
         if (set.ticketsTypes === undefined || set.ticketsTypes.values.length === 0)
             set.ticketsTypes = DEFAULT_SET.ticketsTypes
-
-        if (set.ticketsTags !== undefined) {
+        
+        // This is for backward compatibility in case tag value is plain number, not str number in brackets.
+        if (set.ticketsTags !== undefined)
             set.ticketsTags.values = set.ticketsTags.values.map(x => x.toString().includes('(') ? x : `(${x})`)
-        }
     }
     return customersActivitySets
 }
