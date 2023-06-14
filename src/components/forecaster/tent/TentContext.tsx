c3856d4ec3bbf3d5363ca740ce4aae2ea7d6e205
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-06-14 23:51:44 +0400

    drop forecaster

--- src/components/forecaster/tent/TentContext.tsx
+++
@@ -1,7 +0,0 @@
-import React, { createContext, useContext } from 'react'
-
-export const TentIdContext = createContext<string>('')
-
-export function useTentId() {
-    return useContext(TentIdContext)
-}
