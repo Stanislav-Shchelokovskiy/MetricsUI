4e32e7e42a52011fea3d40b6c37d54c54317e66b
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-07-06 19:29:51 +0400

    add dev ports

--- src/components/common/EndPoint.tsx
+++ src/components/common/EndPoint.tsx
@@ -1,7 +1,7 @@
 const baseEndPoint = 'https://ubuntu-support.corp.devexpress.com'
 //const baseEndPoint = 'http://localhost'
 
-export const FORECASTER_END_POINT = baseEndPoint + '/Forecaster'
-export const SUPPORT_METRICS_END_POINT = baseEndPoint + '/SupportMetrics'
-export const COST_METRICS_END_POINT = baseEndPoint + '/CostMetrics'
-export const QUERY_SERVICE_END_POINT = baseEndPoint + '/QueryService'
+export const FORECASTER_END_POINT = baseEndPoint + '/Forecaster' //:11000
+export const SUPPORT_METRICS_END_POINT = baseEndPoint + '/SupportMetrics' //:11001
+export const COST_METRICS_END_POINT = baseEndPoint + '/CostMetrics' //:11002
+export const QUERY_SERVICE_END_POINT = baseEndPoint + '/QueryService' //:11005
