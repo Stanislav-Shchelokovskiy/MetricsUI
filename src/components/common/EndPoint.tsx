9043b46e4dd87cf396d21dfe8e960e8e1255ffac
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-07-07 12:23:44 +0400

    change end points

--- src/components/common/EndPoint.tsx
+++ src/components/common/EndPoint.tsx
@@ -1,7 +1,11 @@
 const baseEndPoint = 'https://ubuntu-support.corp.devexpress.com'
-//const baseEndPoint = 'http://localhost'
+export const FORECASTER_END_POINT = baseEndPoint + '/v1/Forecaster'
+export const SUPPORT_METRICS_END_POINT = baseEndPoint + '/v1/SupportMetrics'
+export const COST_METRICS_END_POINT = baseEndPoint + '/v1/CostMetrics'
+export const QUERY_SERVICE_END_POINT = baseEndPoint + '/v1/QueryService'
 
-export const FORECASTER_END_POINT = baseEndPoint + '/Forecaster' //:11000
-export const SUPPORT_METRICS_END_POINT = baseEndPoint + '/SupportMetrics' //:11001
-export const COST_METRICS_END_POINT = baseEndPoint + '/CostMetrics' //:11002
-export const QUERY_SERVICE_END_POINT = baseEndPoint + '/QueryService' //:11005
+// const baseEndPoint = 'http://localhost'
+// export const FORECASTER_END_POINT = baseEndPoint + ':11000'
+// export const SUPPORT_METRICS_END_POINT = baseEndPoint + ':11001'
+// export const COST_METRICS_END_POINT = baseEndPoint + ':11002'
+// export const QUERY_SERVICE_END_POINT = baseEndPoint + ':11005'
