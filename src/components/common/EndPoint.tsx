9252491a8887b659e59dd8a2d44a4cd853596b85
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-07-18 17:42:00 +0400

    fix end points

--- src/components/common/EndPoint.tsx
+++ src/components/common/EndPoint.tsx
@@ -1,15 +1,15 @@
-// const baseEndPoint = 'https://ubuntu-support.corp.devexpress.com'
-// export const FORECASTER_END_POINT = baseEndPoint + '/v1/Forecaster'
-// export const SUPPORT_METRICS_END_POINT = baseEndPoint + '/v1/SupportMetrics'
-// export const COST_METRICS_END_POINT = baseEndPoint + '/v1/CostMetrics'
-// export const QUERY_SERVICE_END_POINT = baseEndPoint + '/v1/QueryService'
-// export const MSID_REDIRECT = baseEndPoint
+const baseEndPoint = 'https://ubuntu-support.corp.devexpress.com'
+export const FORECASTER_END_POINT = baseEndPoint + '/v1/Forecaster'
+export const SUPPORT_METRICS_END_POINT = baseEndPoint + '/v1/SupportMetrics'
+export const COST_METRICS_END_POINT = baseEndPoint + '/v1/CostMetrics'
+export const QUERY_SERVICE_END_POINT = baseEndPoint + '/v1/QueryService'
+export const MSID_REDIRECT = baseEndPoint
 
 
-const baseEndPoint = 'http://localhost'
-export const FORECASTER_END_POINT = baseEndPoint + ':11000'
-export const SUPPORT_METRICS_END_POINT = baseEndPoint + ':11001'
-export const COST_METRICS_END_POINT = baseEndPoint + ':11002'
-export const QUERY_SERVICE_END_POINT = baseEndPoint + ':11005'
+// const baseEndPoint = 'http://localhost'
+// export const FORECASTER_END_POINT = baseEndPoint + ':11000'
+// export const SUPPORT_METRICS_END_POINT = baseEndPoint + ':11001'
+// export const COST_METRICS_END_POINT = baseEndPoint + ':11002'
+// export const QUERY_SERVICE_END_POINT = baseEndPoint + ':11005'
 
-export const MSID_REDIRECT = baseEndPoint + ':3000'
+// export const MSID_REDIRECT = baseEndPoint + ':3000'
