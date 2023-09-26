b21304c8a6f16f0fdad3b93190305dfcfe453a85
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-09-26 17:59:48 +0400

    extract TaskProgressNotifier

--- src/components/common/components/TaskButton.tsx
+++ src/components/common/components/TaskButton.tsx
@@ -1,128 +1,26 @@
-import React, { useCallback, useReducer } from 'react'
-import { AnyAction } from '@reduxjs/toolkit'
-import { Toast } from 'devextreme-react/toast'
+import React from 'react'
+import TaskProgressNotifier, { TaskProgressNotifierProps, TaskStarter } from './TaskProgressNotifier'
 import LoadIndicator from '../../common/components/LoadIndicator'
 import Button from '../../common/components/Button'
 
-interface Props {
-    className: string
+interface ButtonProps {
     icon: string
     hint: string
-    task: (
-        dispatchTaskState: (started: boolean) => void,
-        onSuccess: (message: string) => void,
-        onError: (message: string) => void,
-    ) => Promise<void>
 }
 
-interface ToastConfig {
-    message: string
-    visible: boolean
-    displayTime: number
-    type: any
+export default function TaskButton(props: ButtonProps & TaskProgressNotifierProps) {
+    return <TaskProgressNotifier
+        {...props}
+        child={SpinnerButton}
+    />
 }
 
-const defaultToastConfig = {
-    visible: false,
-    message: '',
-    displayTime: 3000,
-    type: 'error',
+function SpinnerButton(props: ButtonProps & TaskStarter) {
+    return props.taskStarted === true ?
+        <LoadIndicator width={36} height={32} /> :
+        <Button
+            icon={props.icon}
+            hint={props.hint}
+            onClick={props.startTask} />
 }
 
-interface TaskButtonState {
-    taskStarted: boolean
-    toastConfig: ToastConfig
-}
-
-const INITIAL_STATE: TaskButtonState = {
-    taskStarted: false,
-    toastConfig: defaultToastConfig
-}
-
-const CHANGE_TASK_STATUS = 'task_started'
-const CHANGE_TOAST_CONFIG = 'toast_config'
-
-
-function stateReducer(state: TaskButtonState, action: AnyAction): TaskButtonState {
-    switch (action.type) {
-        case CHANGE_TASK_STATUS:
-            return {
-                ...state,
-                taskStarted: action.payload,
-            }
-        case CHANGE_TOAST_CONFIG:
-            return {
-                ...state,
-                toastConfig: action.payload
-            }
-        default:
-            return state
-    }
-}
-
-const changeTaskStatus = (started: boolean): AnyAction => {
-    return {
-        type: CHANGE_TASK_STATUS,
-        payload: started
-    }
-}
-
-const changeToastConfig = (next: ToastConfig): AnyAction => {
-    return {
-        type: CHANGE_TOAST_CONFIG,
-        payload: next
-    }
-}
-
-
-export default function TaskButton(props: Props) {
-    const [state, stateDispatch] = useReducer(stateReducer, INITIAL_STATE)
-
-    const onError = useCallback(
-        (message: string) => {
-            stateDispatch(changeToastConfig({
-                displayTime: 4000,
-                type: 'error',
-                message: message,
-                visible: true,
-            }));
-        }, [])
-
-    const onSuccess = useCallback(
-        (message: string) => {
-            stateDispatch(changeToastConfig({
-                displayTime: 2000,
-                type: 'success',
-                message: message,
-                visible: true,
-            }));
-        }, [])
-
-
-    const onHiding = useCallback(() => stateDispatch(changeToastConfig(defaultToastConfig)), [])
-    const dispatchTaskState = useCallback((started: boolean) => stateDispatch(changeTaskStatus(started)), [])
-
-    const task = props.task
-
-    const startTask = useCallback(() => {
-        (async () => {
-            await task(dispatchTaskState, onSuccess, onError)
-        })();
-    }, [dispatchTaskState, onError, onSuccess, task])
-
-    return (
-        <div className={props.className}>
-            {state.taskStarted === true ?
-                <LoadIndicator width={36} height={32} /> :
-                <Button
-                    
-                    icon={props.icon}
-                    hint={props.hint}
-                    onClick={startTask} />}
-            <Toast
-                {...state.toastConfig}
-                onHiding={onHiding}
-            />
-        </div>
-    )
-}
