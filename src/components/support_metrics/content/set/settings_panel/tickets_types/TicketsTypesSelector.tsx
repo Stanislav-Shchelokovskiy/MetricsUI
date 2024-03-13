aded7ca446b16b96d6f1e62870178e922a688375
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2024-03-13 17:37:24 +0400

    TicketsTypesSelector: use default from DEFAULT_SET

--- src/components/support_metrics/content/set/settings_panel/tickets_types/TicketsTypesSelector.tsx
+++ src/components/support_metrics/content/set/settings_panel/tickets_types/TicketsTypesSelector.tsx
@@ -1,7 +1,8 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import MultiOptionSelector from '../../../../../common/components/MultiOptionSelector'
 import { SupportMetricsStore } from '../../../../store/Store'
+import { DEFAULT_SET } from '../../../../store/sets/Defaults'
 import { changeTicketsTypes, changeTicketsTypesInclude } from '../../../../store/actions/TicketsTypes'
 import { fetchTicketsTypes, TicketsType } from '../../../../network_resource_fetcher/tickets/FetchTicketsTypes'
 import { useSetTitle } from '../../../../../common/components/multiset_container/set/SetContext'
@@ -14,7 +15,7 @@ export default function TicketsTypesSelector() {
     const value = useSelector((store: SupportMetricsStore) => ticketsTypesSelector(store, setTitle))
     const onValueChange = (allValues: Array<TicketsType>, values: Array<number>) => changeTicketsTypes({ stateId: setTitle, data: values })
     const onIncludeChange = (include: boolean) => changeTicketsTypesInclude({ stateId: setTitle, data: include })
-    const defaultValue = useMemo(() => [1], [])
+    const defaultValue = DEFAULT_SET.ticketsTypes?.values
     const decompositionArgs = setDecomposition(setTitle, ticketsTypesSelectorName)
 
     return <MultiOptionSelector<TicketsType, number>
